a702ee5755dcd5c5f55eb2a26d73775b32ad49b3
Pedro Enrique Tapia Duran <pedritotapita@hotmail.com>
2019-04-30 10:49:36 -0500

    5subir

--- js/acciones.js
+++ js/acciones.js
@@ -1,15 +1,15 @@
-//JavaScript
+// JavaScript Document
 
 $(document).ready(function(e){
-document.addEventListener("deviceready",function(){
-
-$('#beep').tap(function(){
-navigator.notification.beep(3);
-}); //tap beep
-
-$('##vibrar').tap(function(){
-navigator.notification.vibrate(10000);
-}); //tap vibrar
-
-},false); //deviceready
-}); //ready
+	document.addEventListener("deviceready",function(e){
+		
+		$('#beep').tap(function(){
+		navigator.notification.beep(3);
+		});//tap beep
+		
+		$('#vibrar').tap(function(){
+		navigator.notification.vibrate(1000);
+		});//tap vibrar
+		
+	},false);//deviceready
+});//ready
